60f4bb1b923c3f4a6afd297be1b5af552a2fb809
Lauren West <laurenwest12@gmail.com>
2018-08-22 00:54:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -29,6 +29,7 @@ function onKeyDownHandler(event){
 
 function init() {
   const body = document.querySelector('body')
+  body.addEventListener('keydown', (event) => onKeyDownHandler())
 }
 
 init()
