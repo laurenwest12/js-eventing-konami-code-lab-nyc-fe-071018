afff226d4d4b77a6984d7f82417d1b79ea8662c7
Lauren West <laurenwest12@gmail.com>
2018-08-22 00:24:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,6 +5,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 //We need to keep track of where we are in the combo
 
 function init() {
+  //tracker keeps track of where we are in an array
+  const tracker = 0
   // Write your JavaScript code inside the init() function
-
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', (event) => {
+    if (event.which === code[tracker]){
+      console.log('Success!')
+      tracker++;
+    } else {
+      console.log('Failure')
+      tracker = 0
+    }
+  })
 }
